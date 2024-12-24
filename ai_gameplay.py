e5813cc464fada975ced7cc8b24b11eef61decbb
Shakib Hossen <64243974+xaakib@users.noreply.github.com>
2024-12-24 19:10:38 +0600

    update

--- ai_gameplay.py
+++ ai_gameplay.py
@@ -3,7 +3,7 @@ import random
 
 
 while True:
-    print('started function')
+    print('started streaming............')
     pyautogui.sleep(4)
   
    
@@ -17,7 +17,7 @@ while True:
 
     pyautogui.hotkey("ctrl","1"),
     print('timer started.......')
-    timecalculat = random.randint(16* 60,26* 60)
+    timecalculat = random.randint(14* 60,16* 60)
     print(timecalculat)
     pyautogui.sleep(timecalculat)
     pyautogui.hotkey("ctrl","1"),
@@ -25,12 +25,18 @@ while True:
 
 #browser ending stream
     pyautogui.click(x=1818, y=783)
+    print('Stoped streaming............')
     pyautogui.sleep(120)
+   
 #dismiss.....
     pyautogui.click(x=612, y=801)
 #analysis
     pyautogui.sleep(30)
     pyautogui.click(x=244, y=436)
+    t = random.randint(1* 60,3* 60)
+    print(t)
+    pyautogui.sleep(t)
+    pyautogui.sleep(t)
 
 #switch application
 
@@ -39,6 +45,34 @@ while True:
     pyautogui.sleep(5)
     pyautogui.click(x=1635, y=601)
 #started stream again
+
+
+#recoding program
+
+# while True:
+#     print('started function')
+#     pyautogui.sleep(4)
+  
+   
+
+#     print('4 sec waited function')
+#     pyautogui.click(x=1829, y=829)
+#     pyautogui.sleep(2)
+#     pyautogui.hotkey("alt", "tab"),
+   
+# #streaming and autiomation started.......
+
+#     # pyautogui.hotkey("ctrl","1"),
+#     print('timer started.......')
+#     timecalculat = random.randint(8* 60,10* 60)
+#     print(timecalculat)
+#     pyautogui.sleep(timecalculat)
+#     # pyautogui.hotkey("ctrl","1"),
+#     print('stoped car....')
+
+# #browser ending stream
+#     pyautogui.click(x=1829, y=829)
+#     pyautogui.sleep(80)
 
 
 
